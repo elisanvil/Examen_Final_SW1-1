dd97fb9c0fe80c38a9d4b428c2bb56989398a59f
TaiEspol <taiyuchen92286@gmail.com>
2018-01-31 16:26:27 -0500

    Update validador_cedula.py

--- validador_cedula.py
+++ validador_cedula.py
@@ -2,22 +2,22 @@
 import math
 
 def cedula_es_valida(cedula):
-    """Funcion de verificar"""
- 	if len(cedula) != 10:
- 		return "No valida: debe ser de 10 digitos"
- 	else:
- 		if int(cedula[0:2]) > 24:
- 			return "No valida: Los primeros dos digitos no pertenecen a una provincia"
- 		if int(cedula[2]) > 6:
- 			return "No valida: el tercer digito debe ser menor a 6"
- 		coeficientes = '212121212'
- 		resultado = [0] * 9
- 		for i in range(0, 9):
- 			producto = int(cedula[i]) * int(coeficientes[i])
- 			if producto >= 10:
- 				producto -= 9
- 			resultado[i] = producto
- 		digito = int(math.ceil(sum(resultado) / 10.0)) * 10 - sum(resultado)
- 		if digito != int(cedula[9]):
- 			return "No valida: no coincide el digito validador"
- 		return "Valida"
+    """ funcion de validar """
+    if len(cedula) != 10:
+        return "No valida: debe ser de 10 digitos"
+    else:
+        if int(cedula[0:2]) > 24:
+            return "No valida: Los primeros dos digitos no pertenecen a una provincia"
+        if int(cedula[2]) > 6:
+            return "No valida: el tercer digito debe ser menor a 6"
+        coeficientes = '212121212'
+        resultado = [0] * 9
+        for i in range(0, 9):
+            producto = int(cedula[i]) * int(coeficientes[i])
+            if producto >= 10:
+                producto -= 9
+            resultado[i] = producto
+        digito = int(math.ceil(sum(resultado)/10.0))*10 - sum(resultado)
+        if digito != int(cedula[9]):
+            return "No valida: no coincide el digito validador"
+        return "Valida"
